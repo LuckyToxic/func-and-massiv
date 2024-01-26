d1063d592283adf4e6fd29ed67061b7dc0bd7c45
Evgeniy <151203550+LuckyToxic@users.noreply.github.com>
2024-01-26 14:55:42 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,147 @@
+// let a = 3
+
+// console.log(a)
+
+// let x = 2
+
+// let y = +'4'
+
+// console.log(x + y)
+
+// if( x % 2 == 0){
+//     console.log('Ваше число является четным!')
+// }else{
+//     console.log('Ваше число является нечетным!')
+// }
+
+// let arr = [1,2,3,4,5]
+
+// console.log(arr.length)
+
+// for( let i = 0; i < arr.length;i++){
+//     console.log(arr[i])
+// }
+
+// const user = {
+//     name: 'John',
+//     age: 25,
+//     likeCod: 'JavaScript'
+// }
+
+// console.log(user)
+
+// function culcDiff(lx,lz){
+//    return lx - lz
+// }
+// console.log(culcDiff(10,4))
+
+// let arr2 = ['Ешь','Пей','Жуй Орбит']
+
+// for( let i = 0; i < arr2.length;i++){
+//     console.log(`${arr2[i]}!`)
+// }
+
+// arr2.push('Молись')
+
+// console.log(arr2)
+
+// function createLength(x){
+//     return `Длина строки ${x.length}`
+// }
+
+// console.log(createLength('Молиться'))
+
+// let day = prompt('Введите день недели:')
+
+// switch(day){
+//     case понедельник :
+//         alert('Это не выходной день!')
+//         break;
+//     case вторник :
+//         alert('Это не выходной день!') 
+//         break;
+//     case среда:
+//         alert('Это не выходной день!') 
+//         break;
+//     case четверг:
+//         alert('Это не выходной день!') 
+//         break;
+//     case пятница :
+//         alert('Еще немного,держись!') 
+//         break;
+//     case суббота :
+//         alert('Наконец-то ВЫХОДНОЙ!')
+//         break;
+//     case воскресенье :
+//         alert('Выходной конечно,но завтра уже на работу!')
+//     default:
+//         alert('Видимо весело прошли выходные,это даже не день недели!')
+
+// }
+
+// let work = 'Официант'
+
+// work = 'Программист'
+
+// console.log(work)
+
+
+// let num = 5
+
+// do {
+//     console.log(num);
+//     num--;
+// }while( num >= 0)
+
+
+// function calcSum(array){
+//     return array[0] + array[1] + array[2] + array[3] + array[4]
+// }
+
+// console.log(calcSum(arr))
+
+
+// let num = 15
+
+// let result = num > 10 ? 'Число больше 10' : num === 10 ? 'Число равно 10' : 'Число меньше 10'
+
+// console.log(result)
+
+
+// let car = {
+//     brand : 'Kia',
+//     model : 'Rio',
+//     engineOn(){
+//         console.log(`Двигатель автомобиля ${this.brand} ${this.model} заведен!`)
+//     }
+// }
+
+
+// // // car.engineOn()
+
+
+// for(key in car ){
+//     console.log(car)
+// }
+
+
+// let books = [{title:'Пересмешник',author:'Алексей Пехов',year:2009},                  
+//              {title:'Маленький принц',author:'Ануан де Сент-Экзюпери',year:1943},
+//              {title:'Алхимик',author:'Пауло Коэльо',year:1988}]
+
+
+// for( let i = 0; i < books.length;i++){
+//     console.table(books[i])
+// }
+
+// const arrNum = x => {
+//     let arr = []
+//     for(let i = x; i >= 1;i--){
+//         arr.push(i)
+//     }
+//     return arr
+// }
+
+// console.log(arrNum(6))
+
+
